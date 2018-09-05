8f28c131622c288c7508ed8fbe5c64edd0fb3553
Louis Aurick <louisaurick@ntymail.com>
2018-09-05 18:37:41 +0200

    basic posts function

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'posts/index'
+  get 'posts/show'
+  get 'posts/new'
+  get 'posts/create'
+  get 'posts/update'
+  get 'posts/destroy'
   devise_for :users
   resources :users
   root 'users#index'
