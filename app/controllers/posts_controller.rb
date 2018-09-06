458859098413d2b71ceac42354d2cc35bb7fadeb
Louis Aurick <louisaurick@ntymail.com>
2018-09-06 10:34:55 +0200

    basic post without controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,19 +1,13 @@
 class PostsController < ApplicationController
-  def index
-  end
+  def index; end
 
-  def show
-  end
+  def show; end
 
-  def new
-  end
+  def new; end
 
-  def create
-  end
+  def create; end
 
-  def update
-  end
+  def update; end
 
-  def destroy
-  end
+  def destroy; end
 end
