74102681347385a501098942587b9eec491644af
Louis Aurick <louisaurick@ntymail.com>
2018-09-07 19:35:54 +0200

    crud posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,9 +5,48 @@ class PostsController < ApplicationController
     @posts = Post.order(:created_at)
   end
 
-  def show; end
+  def show
+    @post = Post.find(params[:id])
+  end
 
-  def update; end
+  def new
+    @post = Post.new
+  end
 
-  def destroy; end
+  def create
+    @post = Post.new(post_params)
+    @post.user = User.last
+    if @post.save
+      flash[:success] = "L'article a été publié!"
+      redirect_to @post
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update_attributes(post_params)
+      flash[:success] = "L'article a été édité!"
+      redirect_to @post
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    Post.find(params[:id]).destroy
+    flash[:success] = "L'articlé a été supprimé!"
+    redirect_to posts_url
+  end
+
+  private
+
+  def post_params
+      params.require(:post).permit(:title, :category, :content)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,6 @@ Rails.application.routes.draw do
   get 'static_pages/music'
   get 'static_pages/about'
   devise_for :users
-  resources :users
   resources :posts
   root 'static_pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
