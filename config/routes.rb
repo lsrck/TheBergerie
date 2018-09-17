3dbd77edca25b48c5082732c6ff23889b8471674
Louis Aurick <louisaurick@ntymail.com>
2018-09-17 15:58:46 +0200

    global layout

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   get 'static_pages/cinema'
   get 'static_pages/music'
   get 'static_pages/about'
-  get '/static_pages/:id', to: 'static_pages#show'
+  get '/static_pages/:id', to: 'static_pages#show', as: "article"
   devise_for :users
   resources :posts
   root 'static_pages#home'
